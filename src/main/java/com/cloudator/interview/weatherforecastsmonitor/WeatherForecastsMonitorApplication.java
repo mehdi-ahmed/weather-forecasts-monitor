b9023ae88fa76c2d804dbd644e9993eb92eae706
Your Name <ahmed.mehdi.2009@gmail.com>
2019-01-23 10:09:50 +0100

    first commit

---
+++ src/main/java/com/cloudator/interview/weatherforecastsmonitor/WeatherForecastsMonitorApplication.java
@@ -0,0 +1,14 @@
+package com.cloudator.interview.weatherforecastsmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherForecastsMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherForecastsMonitorApplication.class, args);
+	}
+
+}
+
